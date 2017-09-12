6bad7adb12bbc09896c682bb0ce4cef7341f8857
isybai <9sennheiser9@gmail.com>
2017-09-12 14:20:55 +0600

    minor changes

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -9,7 +9,7 @@ import { MdInputModule } from '@angular/material';
 import { MdSelectModule } from '@angular/material';
 import { MdSidenavModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MdTableModule } from '@angular/material';
 
 import { RoutingModule } from './routing.module';
 import { SharedModule } from './shared/shared.module';
@@ -86,7 +86,8 @@ import { PjvlsComponent } from './nomenclature/pjvls/pjvls.component';
     MdSelectModule,
     MdSidenavModule,
     MdListModule,
-    MdGridListModule
+    MdGridListModule,
+    MdTableModule
   ],
   providers: [
     AuthService,
--- client/app/contragency/medics/medics.component.ts
+++ client/app/contragency/medics/medics.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
+import { DataSource } from '@angular/cdk/collections';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+
 import { MedicService } from '../../services/medic.service';
 import { ToastComponent } from '../../shared/toast/toast.component';
 
@@ -11,6 +15,8 @@ import { ToastComponent } from '../../shared/toast/toast.component';
   styleUrls: ['./medics.component.css']
 })
 export class MedicsComponent implements OnInit {
+  displayedColumns = ['name', 'sur', 'dob', 'spec', 'hos', 'type', 'local', 'workTime', 'tel'];
+  dataSource = new ExampleDataSource();
 
   medic = {};
   medics = [];
@@ -93,5 +99,36 @@ export class MedicsComponent implements OnInit {
       );
     }
   }
+}
 
+export interface Element {
+  name: string;
+  sur: string;
+  dob: string;
+  spec: string;
+  hos: string;
+  type: string;
+  local: string;
+  workTime: string;
+  tel: number; 
+}
+
+const data: Element[] = [
+  {name:'Alena', sur: 'Volkova', dob: '12.1.85', spec:'Акушер', hos: 'Hydrogen', type: 'Клиника', local: 'Первомайский район', workTime:'8:00 - 20:00', tel: 552007324},
+  {name:'Olga', sur: 'Volkova', dob: '11.2.88', spec:'Андролог', hos: 'Hydrogen', type: 'Клиника', local: 'Свердловский район',  workTime:'8:00 - 20:00', tel: 552007324}
+];
+
+/**
+ * Data source to provide what data should be rendered in the table. The observable provided
+ * in connect should emit exactly the data that should be rendered by the table. If the data is
+ * altered, the observable should emit that new set of data on the stream. In our case here,
+ * we return a stream that contains only one set of data that doesn't change.
+ */
+export class ExampleDataSource extends DataSource<any> {
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<Element[]> {
+    return Observable.of(data);
+  }
+
+  disconnect() {}
 }
